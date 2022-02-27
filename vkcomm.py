514e269a2dd3613f8691d3465e51999edefa0840
TerPackZ <100250045+TerPackZ@users.noreply.github.com>
2022-02-27 20:13:49 +0600

    Update vkcomm.py

--- vkcomm.py
+++ vkcomm.py
@@ -26,21 +26,10 @@ choose = input('--> ')
 
 if choose == "yes":
     os.system("termux-open-url 'https://t.me/TerPackZ'")
-    token = input("Введите токен: ")
-    user_id = input("Введите айди страницы: ")
-    posts_id = input("Введите айди поста: ")
-    msgs = input("Введите сообщение: ")
-    session = vk.Session(access_token=token)
-    apivk = vk.API(session, v=5.95)
-while True:
-    try:
-        print(apivk.wall.createComment(owner_id=user_id, post_id=posts_id, message=msgs,
-                                       guid=random.randint(0, 9999999999)))
-    except:
-        pass
-    time.sleep(3)
-
+    print("Если вы подписались, перезапустите скрипт :)")
+    exit()
 else:
+
     token = input("Введите токен: ")
     user_id = input("Введите айди страницы: ")
     posts_id = input("Введите айди поста: ")
